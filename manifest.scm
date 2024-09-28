be4ac0db86339d22ccf05dbbad0327067fdd7677
fronx <fronx@wurmus.de>
2024-09-28 17:17:32 +0200

    Update GUIX manifest

--- manifest.scm
+++ manifest.scm
@@ -2,6 +2,16 @@
              (gnu packages base)
              (gnu packages compression)
              (gnu packages guile)
-             (gnu packages guile-xyz))
+             (gnu packages guile-xyz)
+             (gnu packages gnupg))
 
-(packages->manifest (list guile-next guile-hoot gnu-make zip))
+(packages->manifest
+ (list guile-next
+       guile-hoot
+       gnu-make
+       zip
+       guile-goblins
+       guile-json-4
+       guile-fibers
+       guile-gcrypt
+       guile-readline))
